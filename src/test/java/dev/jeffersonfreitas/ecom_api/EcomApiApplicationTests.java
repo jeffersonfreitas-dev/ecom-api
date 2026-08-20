76e0c6ed88673073b36159ecbb6fca6f1ed2600d
jeffersonfreitas-dev <jefferson.dev21@gmail.com>
2026-08-20 19:35:12 -0300

    add spring boot

---
+++ src/test/java/dev/jeffersonfreitas/ecom_api/EcomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeffersonfreitas.ecom_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
